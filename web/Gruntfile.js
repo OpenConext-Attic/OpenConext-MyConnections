83e1bc8d2748fd3ed58a09a36c0555581f8af8cc
Martin Roest <roest.martin@gmail.com>
2015-11-18 15:30:10 +0100

    Initial commit

---
+++ web/Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+    grunt.initConfig({
+
+        // sass
+        sass: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    style: 'nested'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'scss/',
+                        src: ['**/*.scss', '!style.scss'],
+                        dest: 'css/',
+                        ext: '.css'
+                    },
+                    {src: 'scss/style.scss', dest: 'css/style.css'}
+                ]
+            },
+            min: {
+                options: {
+                    sourceMap: false,
+                    outputStyle: 'compressed'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'scss/',
+                        src: ['**/*.scss', '!style.scss'],
+                        dest: 'css/',
+                        ext: '.min.css'
+                    },
+                    {src: 'scss/style.scss', dest: 'css/style.min.css'}
+                ]
+            }
+        },
+
+        //watcher project
+        watch: {
+            css: {
+                files: ['scss/**/*.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'js/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass:dev', 'sass:min']);
+};
