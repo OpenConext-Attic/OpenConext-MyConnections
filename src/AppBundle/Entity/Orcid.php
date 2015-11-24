c4192e86e58f93406e2bbf78be90e48b254a1ff7
Martin Roest <roest.martin@gmail.com>
2015-11-24 14:38:45 +0100

    Added entity for ORCID MySQL table.

---
+++ src/AppBundle/Entity/Orcid.php
@@ -0,0 +1,75 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="orcid",
+ *     indexes={@ORM\Index(name="value_idx", columns={"value"})}
+ * )
+ * @ExclusionPolicy("all")
+ */
+class User
+{
+    /**
+     * @var string
+     * @ORM\Id()
+     * @ORM\Column(
+     *     type="string",
+     *     length=255,
+     *     name="key",
+     *     nullable=false
+     * )
+     * @Expose()
+     */
+    protected $key;
+
+    /**
+     * @var string
+     * @ORM\Column(
+     *     type="string",
+     *     length=255,
+     *     name="value",
+     *     nullable=false
+     * )
+     * @Expose()
+     */
+    protected $value;
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+}
