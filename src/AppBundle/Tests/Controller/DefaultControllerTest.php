2c580455d805689d602beb3eaa19002c1f122ccc
Martin Roest <roest.martin@gmail.com>
2015-12-21 14:23:51 +0100

    Add mock user object in order to test the index page.

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,21 +2,105 @@
 
 namespace AppBundle\Tests\Controller;
 
+use AppBundle\Security\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testRedirectToLogin()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
         $this->assertTrue(
             $client
                 ->getResponse()
                 ->isRedirect('/login')
         );
+    }
 
-        //$this->assertContains('My Connections', $crawler->filter('#container h1')->text());
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $userMock = $this->getMockUser();
+        static::$kernel->getContainer()
+            ->set(
+                'app.user',
+                $userMock
+            );
+
+        $crawler = $client->request('GET', '/');
+        $this->assertFalse(
+            $client
+                ->getResponse()
+                ->isRedirect('/login')
+        );
+
+        $this->assertContains(
+            $userMock->getDisplayName(),
+            $crawler
+                ->filter('div.loginname')
+                ->text()
+        );
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertContains(
+            'My Connections',
+            $crawler
+                ->filter('div.page-title > h1')
+                ->text()
+        );
+
+        $this->assertContains(
+            'Login',
+            $crawler
+                ->filter('div.modal div.modal-inner > h1')
+                ->text()
+        );
+
+        $link = $crawler
+            ->filter('p.modal-content > a.btn')
+            ->eq(0)
+            ->link();
+
+        $this->assertContains(
+            '/auth',
+            $link->getUri()
+        );
+    }
+
+    public function getMockUser()
+    {
+        $user = $this->getMockBuilder(User::class)
+            ->disableOriginalConstructor()
+            ->setMethods(
+                [
+                    'isLoggedIn',
+                    'getDisplayName',
+                    'getUid',
+                    'getUsername'
+                ]
+            )
+            ->getMock();
+
+        $user->expects($this->any())
+            ->method('isLoggedIn')
+            ->will($this->returnValue(TRUE));
+        $user->expects($this->any())
+            ->method('getDisplayName')
+            ->will($this->returnValue('TestUser'));
+        $user->expects($this->any())
+            ->method('getUid')
+            ->will($this->returnValue('test'));
+        $user->expects($this->any())
+            ->method('getUsername')
+            ->will($this->returnValue('testuser'));
+
+        return $user;
     }
 }
